af315af65ac5f819b656d8f05c9ab8224e81a090
Klaus Fl <kfleerko@thoughtworks.com>
2014-09-13 18:43:42 +0200

    Stores recordings in remoteStorage

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,11 +9,17 @@
  */
 angular.module('voiceMemoRsApp')
   .controller('MainCtrl', function ($scope) {
+
     var setupRecording = function () {
       var mediaConstraints = {
         audio: true
       };
       navigator.getUserMedia(mediaConstraints, onMediaSuccess, onMediaError);
+    };
+
+    var setupRemoteStorage = function () {
+      remoteStorage.access.claim('voicememos', 'rw');
+      remoteStorage.displayWidget();
     };
 
     $scope.startRecording = function () {
@@ -27,16 +33,19 @@ angular.module('voiceMemoRsApp')
     };
 
     $scope.saveRecording = function () {
+      remoteStorage.voicememos.addMemo($scope.memoTitle, $scope.audioRecording).then(function () {
+        console.log("save completed", arguments);
+      });
     };
 
     $scope.recording = false;
     setupRecording();
+    setupRemoteStorage();
 
     function onMediaSuccess(stream) {
       $scope.mediaRecorder = new MediaStreamRecorder(stream);
       $scope.mediaRecorder.mimeType = 'audio/ogg';
       $scope.mediaRecorder.ondataavailable = function (blob) {
-        // POST/PUT "Blob" using FormData/XHR2
         var blobURL = URL.createObjectURL(blob);
         console.log('data available', blob);
         $scope.audioRecording = blob;
---
+++ app/scripts/remotestorage-voicememos.js
@@ -0,0 +1,77 @@
+RemoteStorage.defineModule('voicememos', function(privateClient, publicClient) {
+
+  // Define a common data type using JSON Schema
+  privateClient.declareType('voicememo', {
+    "description": "A memo in spoken language",
+    "type": "object",
+    "properties": {
+      "id": {
+        "type": "string",
+        "format": "id"
+      },
+      "title": {
+        "type": "string"
+      },
+      "file": {
+        "type": "object",
+        "properties": {
+          "mimeType": {
+            "type": "string"
+          },
+          "size": {
+            "type": "number"
+          },
+          "url": {
+            "type": "string",
+            "format": "uri"
+          }
+        },
+        "required": ["mimeType", "size", "url"]
+      }
+    },
+    "required": ["id"]
+  });
+
+  var handleError = function (error) {
+    console.log(error);
+  };
+
+  var saveRecording = function (id, blob) {
+    var promise = promising();
+    var recordingName = 'recordings/' + id;
+    var fileReader = new FileReader();
+
+    fileReader.onload = function() {
+      privateClient.storeFile(blob.type, recordingName, this.result).then(function() {
+        promise.fulfill(privateClient.getItemURL(recordingName));
+      });
+    };
+    fileReader.readAsArrayBuffer(blob);
+
+    return promise;
+  };
+
+  var addMemo = function (title, blob) {
+    var id = new Date().getTime().toString();
+
+    var saveMemo = function (recordingUrl) {
+      return privateClient.storeObject('voicememo', id, {
+        id: id,
+        title: title || "",
+        file: {
+          mimeType: blob.type,
+          size: blob.size,
+          url: recordingUrl
+        }
+      });
+    };
+
+    return saveRecording(id, blob).then(saveMemo).then(function () { }, handleError);
+  };
+
+  return {
+    exports: {
+      addMemo: addMemo
+    }
+  };
+});
