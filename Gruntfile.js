da2c28df60d7756940310ef52481cec7b1efe167
Klaus Fl <kfleerko@thoughtworks.com>
2014-09-22 16:23:26 +0200

    Use bower overrides to include MediaStreamRecorder

--- Gruntfile.js
+++ Gruntfile.js
@@ -322,11 +322,6 @@ module.exports = function (grunt) {
           cwd: 'bower_components/bootstrap/dist',
           src: 'fonts/*',
           dest: '<%= yeoman.dist %>'
-        },{
-          expand: true,
-          cwd: 'bower_components/MediaStreamRecorder',
-          src: '{,*/}*.js',
-          dest: '<%= yeoman.dist %>/bower_components/MediaStreamRecorder'
         }]
       },
       styles: {
